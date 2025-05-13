3c92a621bb9983042ec4e5f7e8d35262153c7117
Eric <rasliche@gmail.com>
2025-05-13 12:34:41 -0400

    light mode and theme toggle (#51)
    * light mode and theme toggle
    
    * quick navbar button fix
    
    closes #12

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="navbar bg-primary text-primary-content">
-    <button to="/" class="btn btn-ghost text-xl navbar-start">
-      Travel Log
-    </button>
+    <div class="navbar-start">
+      <button to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </button>
+    </div>
     <div class="navbar-end">
+      <AppThemeToggle />
       <a class="btn btn-accent">Sign In With Github <Icon name="tabler:brand-github" size="24" /></a>
     </div>
   </div>
