4d6d9768153796945739568412a410d1e66d99f7
Eric Raslich <rasliche@gmail.com>
2025-05-13 12:11:09 -0400

    add landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <button to="/" class="btn btn-ghost text-xl navbar-start">
+      Travel Log
+    </button>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign In With Github <Icon name="tabler:brand-github" size="24" /></a>
+    </div>
+  </div>
+</template>
