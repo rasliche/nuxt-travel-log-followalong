ef3ca68769002a6f5031e5844eb9a95351acc116
Eric Raslich <rasliche@gmail.com>
2025-05-14 15:36:07 -0400

    add a loading spinner to auth button

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -10,11 +10,14 @@ async function signIn() {
     provider: "github",
     callbackURL: "/dashboard",
   });
+  loading.value = false;
 }
 </script>
 
 <template>
   <button :disabled="loading" class="btn btn-accent" @click="signIn">
-    Sign In With Github <Icon name="tabler:brand-github" size="24" />
+    Sign In With Github
+    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
   </button>
 </template>
