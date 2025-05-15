6fa44cb9f456d0ec911ae18d4cf2a24eef4126b1
Eric Raslich <rasliche@gmail.com>
2025-05-15 08:30:16 -0400

    error styling and display

--- pages/error.vue
+++ pages/error.vue
@@ -4,5 +4,13 @@ const error = route.query.error || "An unknown error occured.";
 </script>
 
 <template>
-  <div>{{ error }}</div>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center">
+      <span>{{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-primary">
+      Home
+      <Icon name="tabler:arrow-left" size="32" />
+    </NuxtLink>
+  </div>
 </template>
